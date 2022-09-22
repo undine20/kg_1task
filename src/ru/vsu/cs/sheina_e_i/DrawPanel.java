0e399f9001738792d56e014183e32418615828b6
elena <lena_sheina@bk.ru>
2022-09-22 23:11:12 +0300

    Добавлена плавная анимация

--- src/ru/vsu/cs/sheina_e_i/DrawPanel.java
+++ src/ru/vsu/cs/sheina_e_i/DrawPanel.java
@@ -8,11 +8,13 @@ import ru.vsu.cs.sheina_e_i.background.Tree;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 
-public class DrawPanel extends JPanel {
+public class DrawPanel extends JPanel implements ActionListener {
 
     Sun sun = new Sun(240, 180, 150);
     Cloud firstCloud = new Cloud(370, 120, 410);
@@ -22,31 +24,17 @@ public class DrawPanel extends JPanel {
     Tree secondTree = new Tree(240, 400, 120);
     Tree thirdTree = new Tree(110, 380, 90);
     Elephant elephant = new Elephant(-100, 450, 100);
+    Timer timer = new Timer(20, this);
 
     public DrawPanel() {
-        this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if(secondCloud.getX() >= 800) {
-                    secondCloud.setX(-350);
-                } else if (firstCloud.getX() >= 800){
-                    firstCloud.setX(-410);
-                } else if (thirdCloud.getX() >= 800) {
-                    thirdCloud.setX(-200);
-                }
-                firstCloud.setX(firstCloud.getX() + 15);
-                secondCloud.setX(secondCloud.getX() + 15);
-                thirdCloud.setX(thirdCloud.getX() + 15);
-                elephant.setX(elephant.getX() + 5);
-                repaint();
-            }
-        });
+        timer.start();
     }
 
     @Override
     public void paint(Graphics gr) {
         super.paint(gr);
         Graphics2D g = (Graphics2D) gr;
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
         MainPicture.sky(g);
 
@@ -65,5 +53,27 @@ public class DrawPanel extends JPanel {
         thirdTree.print(g);
 
         elephant.paint(g);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(secondCloud.getX() >= 800) {
+            secondCloud.setX(-350);
+        } else if (firstCloud.getX() >= 800){
+            firstCloud.setX(-410);
+        } else if (thirdCloud.getX() >= 800) {
+            thirdCloud.setX(-200);
+        }
+
+        firstCloud.setX(firstCloud.getX() + 1);
+        secondCloud.setX(secondCloud.getX() + 1);
+        thirdCloud.setX(thirdCloud.getX() + 1);
+
+        if (elephant.getX() >= 900) {
+            elephant.setX(-190);
+        }
+        elephant.setX(elephant.getX() + 3);
+
+        repaint();
     }
 }
